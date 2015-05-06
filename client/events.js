5c9f50c248e0e9eee261d942884453ce26dd7dda
Yasar icli <hello@yasaricli.com>
2015-05-06 15:32:40 +0200

    play resume controls game

--- client/events.js
+++ client/events.js
@@ -1,3 +1,12 @@
+Template.game.events({
+  'click .fa-pause': function() {
+    Core.pause();
+  },
+  'click .fa-play': function() {
+    Core.resume();
+  }
+});
+
 Template.loginWithFacebook.events({
   'click .is-facebook:not(.user)': function(e) {
     e.preventDefault();
